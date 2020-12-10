3667ad49912e0b5a5cc24f4e0c41402f9662c2ab
Daniel dos Santos Araujo <denenewton@icloud.com>
2020-12-09 23:59:55 -0300

    mundança nos ejs e css

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 require('./src/database/database')
 const express = require('express');
+const {resolve} = require('path')
 const routeUploads = require('./src/routes/routeUploads')
 const app = express();
 
+app.use(express.static(resolve(__dirname, 'public')));
+app.set('views',resolve(__dirname,'views'));
 app.set('view engine', 'ejs')
 
 app.use('/', routeUploads);
